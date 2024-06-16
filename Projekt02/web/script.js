f8b249f18927f0f56cf061d596463b7774596924
Beutelmeyer Maximilian <40146720230306@litec.ac.at>
2024-06-16 18:29:07 +0200

    first commit

---
+++ Projekt02/web/script.js
@@ -0,0 +1,25 @@
+function addComment() { 
+    let username= document.querySelector("#username").value;
+        let comment= document.querySelector("#com").value;
+        let fehler = document.querySelector("#fehler");
+        
+
+    if(username === "" || comment === ""){
+        document.querySelector("#fehler").innerHTML= "ERROR! Sie müssen in beide Textfleder etwas hinenschreiben";
+        fehler.addComment.innerHTML="Fehler";
+        document.getElementById('#username').value = '';
+        document.getElementById('#com').value = '';
+    } else{
+        document.querySelector("#fehler").innerHTML= "";
+        console.log(username)
+        console.log(comment)
+        document.getElementById("outputfeld").innerHTML += `<div class="comment"><p class="output">${username}</p><p class="output2">${comment}</p></div>`;
+        document.getElementById('username').value = '';
+        document.getElementById('com').value = '';
+    
+    }
+}
+
+
+
+
